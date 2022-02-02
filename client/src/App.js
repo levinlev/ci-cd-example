1460e7df1fe3c3b899ef0494b772359a65707fbc
Lev Levin <levl@diagnosticrobotics.com>
2022-02-02 22:59:27 +0200

    .

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is my second CI/CD
+          This is my first CI/CD
         </p>
       </header>
     </div>
